99e52c9f71638394ec9ab23390f72dc04434dc1c
tmorawiec <viochman@gmail.com>
2020-11-09 06:02:11 +0100

    Część druga. Usuwanie zbędnych funkcji

---
+++ IdentityExample/Startup.cs
@@ -0,0 +1,57 @@
+using IdentityExample.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IdentityExample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<AppDbContext>(config => {
+                config.UseInMemoryDatabase("Memory");
+            });
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+            //TM
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseRouting();
+
+            //TM
+            // who are you ?
+            app.UseAuthentication();
+
+
+            //TM
+            // are you allowed ?
+            app.UseAuthorization();
+
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                //TM
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
